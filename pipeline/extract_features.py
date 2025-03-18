2d6a4f9551012bb81b0ccf24ef26592a900005d3
Adam Podoxin <adam.podoxin@gmail.com>
2025-03-18 16:14:33 -0700

    Start implementing appraisal categories

--- pipeline/extract_features.py
+++ pipeline/extract_features.py
@@ -2,8 +2,43 @@ import sys
 import pandas as pd
 
 
-def extract_all_features(data):
+# From Asher et al. (2009)
+verb_categories = {
+    "inform": ["inform", "notify", "explain"],
+    "assert": ["assert", "claim", "insist"],
+}
+
+
+def extract_interview_verb_categories(data: pd.DataFrame):
+    for category in verb_categories.keys():
+        data[f"interview_{category}_ratio"] = 0
+
+    interview: str = data["Transcript"]
+
+    num_appraisal_verbs = 0
+
+    for token in interview:
+        for category, verbs in verb_categories.items():
+            if token in verbs:
+                data[f"interview_{category}_ratio"] += 1
+                num_appraisal_verbs += 1
+
+    if num_appraisal_verbs > 0:
+        for category in verb_categories.keys():
+            data[f"interview_{category}_ratio"] /= num_appraisal_verbs
+    
     return data
+
+
+ignore_columns = ["Name", "Role", "Transcript", "Resume", "Reason_for_decision", "Job_Description"]
+
+
+def extract_all_features(data):
+    data_with_features = extract_interview_verb_categories(data)
+
+    data_with_features.drop(columns=ignore_columns, axis=1, inplace=True)
+
+    return data_with_features
 
 
 def main():
