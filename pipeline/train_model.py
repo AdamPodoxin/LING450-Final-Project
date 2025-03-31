394042f4f34715ddf209bcb7c4f63dea674d905d
Adam Podoxin <adam.podoxin@gmail.com>
2025-03-31 00:23:14 -0700

    Add sentiment stats

--- pipeline/train_model.py
+++ pipeline/train_model.py
@@ -17,7 +17,15 @@ def main(input_file: str):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = GradientBoostingClassifier(n_estimators=100, random_state=42, max_depth=8, min_samples_leaf=0.03)
+    model = GradientBoostingClassifier(
+        n_estimators=200,
+        learning_rate=0.05,
+        max_depth=6,
+        min_samples_leaf=10,
+        subsample=0.8,
+        random_state=42
+    )
+    
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
